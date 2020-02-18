0a6e823e24cc18002f3e87ce9ef64f05bd4f2530
Harley McDowell <hmcdowell95@gmail.com>
2020-02-18 22:36:30 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,17 @@
+class Genre 
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def songs 
+    Song.all.select {|x| x.genre == self}
+  end
